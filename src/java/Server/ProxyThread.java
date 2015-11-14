c9ea034cca1391cfb789e0eca33c2afc823bf2c9
Frank Sun <erpang007@gmail.com>
2015-11-13 20:29:27 -0800

    not sure why is not working

--- src/java/Server/ProxyThread.java
+++ src/java/Server/ProxyThread.java
@@ -1,19 +1,10 @@
 package Server;
 
-import Utils.Utilities;
-import com.sun.security.ntlm.Server;
 
 import java.io.*;
 import java.net.Socket;
 import java.io.OutputStream;
 import java.io.InputStream;
-import java.net.URL;
-import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
 
 /**
  * Created by chenfs on 11/8/15.
@@ -44,11 +35,13 @@ public class ProxyThread extends Thread {
             String firstLine = readIn.readLine();
             System.out.println(firstLine);
 
-            if (firstLine.toLowerCase().startsWith("connect")) {
+            if (firstLine != null && firstLine.contains("http")) {
+                if (firstLine.toLowerCase().startsWith("connect")) {
 
-            } else {
-                // Non connect branch
-                nonConnect(readIn, firstLine);
+                } else {
+                    // Non connect branch
+                    nonConnect(readIn, firstLine);
+                }
             }
 
         } catch (Exception e) {
@@ -63,22 +56,22 @@ public class ProxyThread extends Thread {
         String requestLine = firstLine;
         StringBuilder fullRequest = new StringBuilder();
 
-        if (firstLine.toUpperCase().contains("HTTP/1.1")) {
-            requestLine = firstLine.replaceAll("HTTP/1.1", "HTTP/1.0").replaceAll("http/1.1", "http/1.0");
-        }
-        fullRequest.append(requestLine + "\r\n");
         try {
             int port = 0;
-            String serverAddr = "";
+            String host = "";
             requestLine = readIn.readLine();
             while (requestLine != null) {
-                if (requestLine.toLowerCase().startsWith("host")) {
+                String temp = requestLine.toLowerCase().trim();
+                if (temp.startsWith("host")) {
                     port = getPort(firstLine, requestLine);
-                    serverAddr = requestLine.substring(6);
-                } else if (requestLine.toLowerCase().startsWith("connection:")) {
+                    host = requestLine.substring(6);
+                    System.out.println("Host in nonConnect is " + host);
+                } else if (temp.startsWith("connection:")) {
                     requestLine = "Connection: close";
-                } else if (requestLine.toLowerCase().startsWith("proxy-connection:")) {
+                } else if (temp.startsWith("proxy-connection:")) {
                     requestLine = "Proxy-connection: close";
+                } else if (temp.contains("http/1.1")) {
+                    requestLine = firstLine.replaceAll("HTTP/1.1", "HTTP/1.0").replaceAll("http/1.1", "http/1.0");
                 } else if (requestLine.equals("")) {
                     break;
                 }
@@ -86,27 +79,44 @@ public class ProxyThread extends Thread {
                 fullRequest.append(requestLine + "\r\n");
                 requestLine = readIn.readLine();
             }
-            //System.out.println("request is " + fullRequest.toString() + "end of request");
-            System.out.println("server is " + serverAddr);
+            System.out.println("First line is " + firstLine);
+            System.out.println("host is " + host);
             System.out.println("port is " + port);
             OutputStream out = socket.getOutputStream();
-            sendToServer(port, serverAddr, fullRequest.toString(), out);
+            sendToServer(port, host, fullRequest.toString(), out);
         } catch (Exception e) {
                 e.printStackTrace();
                 return;
         }
     }
 
-    private void sendToServer(int port, String serverAddr, String fullRequest, OutputStream out) {
+    private void sendToServer(int port, String host, String fullRequest, OutputStream out) {
         Socket socket2Server;
         InputStream fromServer;
         PrintWriter writeToServer;
-
+        System.out.println("request is \n" + fullRequest);
+        System.out.println("Sending to host..........");
         try {
-            socket2Server = new Socket(serverAddr, port);
-            fromServer = socket2Server.getInputStream();
+            socket2Server = new Socket(host, port);
+            fromServer = new BufferedInputStream(socket2Server.getInputStream());
+
             writeToServer = new PrintWriter(socket2Server.getOutputStream());
             writeToServer.write(fullRequest);
+            writeToServer.flush();
+
+            System.out.println("Reading from host..........");
+            byte[] buffer = new byte[1024 * 6];
+            int res = fromServer.read(buffer);
+            System.out.println("read is " + res);
+
+            while (res > 0 || res != -1) {
+                System.out.println("Writing to client......");
+                out.write(buffer, 0, res);
+                out.flush();
+
+                res = fromServer.read(buffer);
+            }
+            System.out.println("Done writing");
         } catch (Exception e) {
             e.printStackTrace();
         }
