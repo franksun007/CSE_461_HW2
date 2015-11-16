965d9cd31b3c93850da2c548e0750428b8001a43
Frank Sun <erpang007@gmail.com>
2015-11-15 20:00:20 -0800

    FUCKING PIECE OF SHIT

--- src/java/Server/ProxyThread.java
+++ src/java/Server/ProxyThread.java
@@ -82,7 +82,7 @@ public class ProxyThread extends Thread {
         } catch (Exception e) {
             closeSocket(this.socket);
             OUTPUT.println("Unexpected exception");
-            e.printStackTrace();
+            OUTPUT.println(e.getMessage());
         }
     }
 
@@ -93,93 +93,87 @@ public class ProxyThread extends Thread {
         ByteBuffer buffer = ByteBuffer.allocate(DEFAULT_PACKET_SIZE);
         String msg502 = "HTTP/1.0 502 BAD GATEWAY\r\n\r\n";
         String msg200 = "HTTP/1.1 200 OK\r\n\r\n";
-
         try {
             proxyChannel = SocketChannel.open();
             if (!proxyChannel.connect(new InetSocketAddress(host, port))) {
                 browserChannel.write(buffer.put(msg502.getBytes("ascii")));
                 buffer.flip();
+                return;
             }
-
             browserChannel.configureBlocking(false);
             proxyChannel.configureBlocking(false);
             selector = Selector.open();
             proxyChannel.register(selector, SelectionKey.OP_READ);
             browserChannel.register(selector, SelectionKey.OP_READ);
-
+            browserChannel.socket().setSoTimeout(1000);
+            proxyChannel.socket().setSoTimeout(1000);
             // Sends the 200 msg to browser
             browserChannel.write(buffer.put(msg200.getBytes("ascii")));
             buffer.flip();
-
             assert (proxyChannel.isConnected() && browserChannel.isConnected());
-
             // Start tunneling
-            while (true) {
-                int readyChannel = selector.select();
-                if (readyChannel == 0) continue;
-                Set<SelectionKey> selectedKeys = selector.selectedKeys();
-                Iterator<SelectionKey> iter = selectedKeys.iterator();
-
-                while (iter.hasNext()) {
-                    SelectionKey key = iter.next();
-
-                    if (key.isReadable()) {
-                        SocketChannel readFromChannel = (SocketChannel) key.channel();
-                        SocketChannel writeToChannel;
-                        if (readFromChannel.equals(proxyChannel)) {
-                            // Reading from proxy and write to client
-                            writeToChannel = browserChannel;
-                        } else {
-                            // Reading from client and write to proxy
-                            writeToChannel = proxyChannel;
-                        }
-                        int readCount = readFromChannel.read(buffer);
-
-                        while (readCount > 0) {
-                            buffer.flip();
-                            while (buffer.hasRemaining()) {
-                                writeToChannel.write(buffer);
+            try {
+                while (true) {
+                    int readyChannel = selector.select();
+                    if (readyChannel == 0) continue;
+                    Set<SelectionKey> selectedKeys = selector.selectedKeys();
+                    Iterator<SelectionKey> iter = selectedKeys.iterator();
+                    while (iter.hasNext()) {
+                        SelectionKey key = iter.next();
+                        if (key.isReadable()) {
+                            SocketChannel readFromChannel = (SocketChannel) key.channel();
+                            SocketChannel writeToChannel;
+                            if (readFromChannel.equals(proxyChannel)) {
+                                // Reading from proxy and write to client
+                                writeToChannel = browserChannel;
+                            } else {
+                                // Reading from client and write to proxy
+                                writeToChannel = proxyChannel;
                             }
-                            buffer.flip();
-                            readCount = readFromChannel.read(buffer);
+                            int readCount = readFromChannel.read(buffer);
+                            while (readCount > 0) {
+                                buffer.flip();
+                                while (buffer.hasRemaining()) {
+                                    writeToChannel.write(buffer);
+                                }
+                                buffer.flip();
+                                readCount = readFromChannel.read(buffer);
+                            }
                         }
+                        iter.remove();
                     }
-                    iter.remove();
                 }
+            } catch(Exception e) {
+                proxyChannel.close();
+                socket.close();
             }
         } catch (Exception e) {
             closeSocket(this.socket);
-            e.printStackTrace();
+            OUTPUT.println(e.getMessage());
         }
     }
 
     private void nonConnect(String host, int port, String fullRequest) {
         try {
-
-
             Socket proxySocket = new Socket(host, port);
             InputStream fromServerToProxy = proxySocket.getInputStream();
-
-            DataOutputStream fromProxyToServer = new DataOutputStream(proxySocket.getOutputStream());
-
-            DataOutputStream fromProxyToClient = new DataOutputStream(this.socket.socket().getOutputStream());
-
+            DataOutputStream fromProxyToServer =
+                    new DataOutputStream(proxySocket.getOutputStream());
+            DataOutputStream fromProxyToClient =
+                    new DataOutputStream(this.socket.socket().getOutputStream());
             fromProxyToServer.write(fullRequest.getBytes("ascii"));
             fromProxyToServer.flush();
-
             byte[] data = new byte[DEFAULT_PACKET_SIZE];
-
             int read = fromServerToProxy.read(data);
             while (read != -1) {
                 fromProxyToClient.write(data, 0, read);
                 fromProxyToClient.flush();
                 read = fromServerToProxy.read(data);
             }
-
             closeSocket(this.socket);
             proxySocket.close();
         } catch (Exception e) {
-            e.printStackTrace();
+            OUTPUT.println(e.getMessage());
         }
     }
 
@@ -187,9 +181,9 @@ public class ProxyThread extends Thread {
         try {
             socket.close();
         } catch (Exception e) {
-            OUTPUT.println("Socket closure failed:");
+            OUTPUT.print("Socket closure failed: ");
             OUTPUT.println(socket);
-            e.printStackTrace();
+            OUTPUT.println(e.getMessage());
         }
     }
 }
